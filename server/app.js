6e3451dc738eff22c5cad287926fafc740114ded
Albin Otterhäll <gusalbiot@student.gu.se>
2021-09-24 14:29:37 +0200

    Läs in data från CSV fil
    Skickar den som JSON.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	const parsedfile = parseCSVfile()
+	res
+		.set({'content-type': 'application/json; charset=utf-8'})
+		.send({'data': parsedfile})
 })
+
+function parseCSVfile() {
+	let readContents = fs
+		.readFileSync("data.csv", {encoding: "utf8"})
+		.toString()
+		.split(/[;\r\n]/);  
+	return readContents;
+}
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
