a1734f9a93ecf6f2df649a89cf211acf1123b479
Krešimir <knogds@gmail.com>
2021-10-14 12:29:43 +0200

    Added comments

--- front-end/d3_v0.3b_heatmap_age_groups/static/js/import_agedata.js
+++ front-end/d3_v0.3b_heatmap_age_groups/static/js/import_agedata.js
@@ -2,10 +2,14 @@
 var age_data;
 var agespan = '20-24 år';
 
+/* Import the age data from the agedata.csv file */
 let import_agedata =
     d3.dsv(';', 'data/agedata.csv')
     .then(d => age_data = d);
 
+/* Get the data based on age group
+   get_age_data_by_county_and_age_group("100+ år")
+       = [ 777, 129, 116, 163, 167, 82, 106, 22, 67, 455, ... ]  */
 function get_age_data_by_age_group(age_group){
     let age_data_by_age_group = [];
     for (const i in age_data){
@@ -18,7 +22,9 @@ function get_age_data_by_age_group(age_group){
     return age_data_by_age_group;
 }
 
-function get_age_data_by_age_group_and_county(county, age_group){
+/* Get the data based on County and age group
+   get_age_data_by_county_and_age_group("Värmlands län", "100+ år") = 109 */
+function get_age_data_by_county_and_age_group(county, age_group){
     var curr_county;      
     var age_data_by_age_group_and_county = null;
     for (const i in age_data){                                               
