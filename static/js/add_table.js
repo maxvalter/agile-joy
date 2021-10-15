861ab6999fd7465508949e4ba9c3978f8cd0f90f
Kim <gushagmib@student.gu.se>
2021-10-15 10:28:28 +0200

    Added add_table

--- static/js/add_table.js
+++ static/js/add_table.js
@@ -1,8 +1,29 @@
 
-/*
-    TODO
-*/
+function add_table(data, append_to) {
+    {
+        var columns = data.columns;
 
-function add_table(append_to) {
-    return null;
+        d3.select(append_to)
+            .append("table")
+            .attr("class", "tabell") 
+            .append("thead")
+            .append("tr")
+            .selectAll("th")
+            .data(columns)
+            .join("th")
+            .text(d => d)
+
+        d3.select(".tabell")
+            .append("tbody")
+        
+        d3.select("tbody")
+            .selectAll("tr")
+            .data(data)
+            .enter()
+            .append("tr")
+            .selectAll("td")
+            .data(d => Object.values(d))
+            .enter().append("td")
+            .text(d => d);
+    }
 };
